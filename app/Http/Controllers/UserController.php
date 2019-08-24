812288bf55945feeabee89e6fede94aeed6a44d2
jose manuel alfonso <jose.manuelchema111@gmail.com>
2019-08-24 13:13:40 +0200

    Registro usuario & install firebase-JWT

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,35 +4,75 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+// Incluye el usuario
+use App\User;
+
 class UserController extends Controller
 {
+    /*
     public function pruebas(Request $request){
         return "Función pruebas de UserController";
     }
-
+    */
     public function register(Request $request){
-
         // Recoger datos del usuario por post
+        // $json = $request->input('name del input', valor si no encuentra el input);
         $json = $request->input('json', null);
         $params = json_decode($json);
+
+        // Array de parametros (si se necesita)
+        // $params_array = json_decode($json, true si array);
         $params_array = json_decode($json, true);
-        var_dump($params_array);die();
+
+        // Limpiar datos
+        $params_array = array_map('trim', $params_array);
 
         // Validar datos
+        // Se usa la clase validator
+        $validate = \Validator::make($params_array, [
+            'name'      => 'required|alpha',
+            'surname'   => 'required|alpha',
+            'email'     => 'required|email|unique:users', // unico:tablaObjetivo
+            'password'  => 'required',
+        ]);
 
-        // Cifrar contraseña
+        if ($validate->fails()){            
+            $data = array(
+                'status'  => 'error',
+                'code'    => 404,
+                'message' => 'El usuario no se ha creado correctamente',
+                'errors'  => $validate->errors()
+            );
+            
+            return response()->json($data, $data['code']);
 
-        // Comprobar si existe el usuario
+        } else {
 
-        // Crear usuario
+            // Cifrar contraseña
+            $pass = password_hash($params->password, PASSWORD_BCRYPT, ['cost' => 4]);            
 
-        $data = array(
-            'status'  => 'error',
-            'code'    => 404,
-            'message' => 'El usuario no se ha creado correctamente'
-        );
+            // Crear usuario
+            $user = new User();
+            $user->name = $params_array['name'];
+            $user->surname = $params_array['surname'];
+            $user->email = $params_array['email'];
+            $user->password = $pass;
+            $user->role = 'USER_ROLE';
+            
+            // Guardar el usuario
+            $user->save();
 
-        return response()->json($data, $data['code']);
+            $data = array(
+                'status'  => 'success',
+                'code'    => 200,
+                'message' => 'El usuario se ha creado correctamente',
+                'errors'  => null,
+                'user'    => $user
+            );
+
+            return response()->json($data, $data['code']);
+
+        }
     }
 
     public function login(Request $request){
